2df747c4f91ec24e256a03def533a366d35fc135
Quinn Reilly <Quinn.Reilly@hotmail.com>
2018-07-13 01:01:08 +0100

    build 0.05

--- js/home.js
+++ js/home.js
@@ -1,4 +1,35 @@
-function myFunction()
+var isOpen = 0;
+var closeSize = 0;
+var openSize = 100;
+var openOpacity = 1;
+var closeOpacity = 0;
+
+function menuClick()
+{    
+    if(isOpen == 0)
+    {
+        isOpen = 1;
+        openMenu();
+    }
+    else if(isOpen == 1)
+    {
+        isOpen = 0;
+        closeMenu();
+    }
+}
+
+function openMenu()
 {
-	
+    var openSizeFinal = openSize + "vh";
+    
+    document.getElementById("mainMenu").style.height = openSizeFinal;
+    document.getElementById("menuItemDiv").style.height = openSizeFinal;
+}
+
+function closeMenu()
+{
+    var closeSizeFinal = closeSize + "vh";
+    
+    document.getElementById("mainMenu").style.height = closeSizeFinal;
+    document.getElementById("menuItemDiv").style.height = closeSizeFinal;
 }
