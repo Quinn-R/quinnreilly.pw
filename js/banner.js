51dc40079f561ec5bfb89861377ca15072a4f9b2
Quinn Reilly <Quinn.Reilly@hotmail.com>
2018-07-22 19:15:39 +0100

    build 0.14.6

---
+++ js/banner.js
@@ -0,0 +1,36 @@
+document.write(
+'<div id="banner">'+
+    '<div id="menu">'+
+    '    <div id="menuBar">'+
+    '        <img id="discord" src="img/icons/discord.png" alt="discord link">'+
+    '        <img id="reddit" src="img/icons/reddit.png" alt="reddit link">'+
+    '        <img id="menuImg" src="img/icons/menu.png" alt="open menu" onClick="menuClick()">'+
+    '    </div>'+
+    '    <div id="mainMenu">'+
+    '        <div id="menuItemDiv">'+
+    '            <div id="menuItem1" class="menuItem" onClick="homeClick()">'+
+    '                <p id="menuItemText1" class="whiteTextMenu">Home</p>'+
+    '            </div>'+
+    '            <div id="menuItem2" class="menuItem" onClick="storiesClick()">'+
+    '                <p id="menuItemText2" class="whiteTextMenu">Stories</p>'+
+    '            </div>'+
+    '            <div id="menuItem3" class="menuItem">'+
+    '                <p id="menuItemText3" class="whiteTextMenu">Videos</p>'+
+    '            </div>'+
+    '            <div id="menuItem4" class="menuItem">'+
+    '                <p id="menuItemText4" class="whiteTextMenu">Projects</p>'+
+    '            </div>'+
+    '            <div id="menuItem5" class="menuItem">'+
+    '                <p id="menuItemText5" class="whiteTextMenu">Index</p>'+
+    '            </div>'+
+    '            <div id="menuItem6" class="menuItem" onClick="linkClick("menuItem4")">'+
+    '                <p id="menuItemText6" class="whiteTextMenu">Contact</p>'+
+    '            </div>'+
+    '        </div>'+
+    '    </div>'+
+    '</div>'+
+    '<div id="innerBanner">'+
+    '    <img id="bannerImage" src="img/banner.png" alt="banner">'+
+    '</div>'+
+'</div>'
+);
